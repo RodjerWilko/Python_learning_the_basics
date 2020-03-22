c47e164a8d5141e3c119d8684d0d0589a6388e90
Максим <ipkochetovmv@gmail.com>
2020-03-22 23:17:41 +0300

    урок 3. wall.  сделано.

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -6,6 +6,16 @@ import simple_draw
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-# TODO здесь ваш код
+
+count = 1
+for i in range(0, 1000, 50):
+    count += 1
+    for j in range(0, 1000, 100):
+        if count % 2 == 1:
+            j -= 50
+        left_bottom = simple_draw.Point(j, i)
+        right_top = simple_draw.Point(j + 100, i + 50)
+        simple_draw.rectangle(left_bottom=left_bottom, right_top=right_top, color=simple_draw.COLOR_DARK_RED, width=2)
+
 
 simple_draw.pause()
