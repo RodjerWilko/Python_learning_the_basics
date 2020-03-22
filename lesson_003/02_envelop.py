9fae6841558e76848d322add60dc6ddd43058756
Максим <ipkochetovmv@gmail.com>
2020-03-22 20:02:24 +0300

    урок 3. envelop. сделано.

--- lesson_003/02_envelop.py
+++ lesson_003/02_envelop.py
@@ -10,17 +10,23 @@
 # Использовать только операторы if/elif/else, можно вложенные
 
 envelop_x, envelop_y = 10, 7
-paper_x, paper_y = 8, 9
+# paper_x, paper_y = 8, 9
 # проверить для
 # paper_x, paper_y = 9, 8
 # paper_x, paper_y = 6, 8
 # paper_x, paper_y = 8, 6
 # paper_x, paper_y = 3, 4
 # paper_x, paper_y = 11, 9
-# paper_x, paper_y = 9, 11
+paper_x, paper_y = 9, 11
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if paper_x <= envelop_x and paper_y <= envelop_y:
+    print("ДА")
+elif paper_x <= envelop_y and paper_y <= envelop_x:
+    print("ДА")
+else:
+    print('НЕТ')
+
 
 # Усложненное задание, решать по желанию.
 # Заданы размеры hole_x, hole_y прямоугольного отверстия и размеры brick_х, brick_у, brick_z кирпича (все размеры
@@ -28,7 +34,7 @@ paper_x, paper_y = 8, 9
 #
 # Определить, пройдет ли кирпич через отверстие (грани кирпича параллельны сторонам отверстия)
 
-# hole_x, hole_y = 8, 9
+hole_x, hole_y = 8, 9
 # brick_x, brick_y, brick_z = 11, 10, 2
 # brick_x, brick_y, brick_z = 11, 2, 10
 # brick_x, brick_y, brick_z = 10, 11, 2
@@ -46,7 +52,21 @@ paper_x, paper_y = 8, 9
 # brick_x, brick_y, brick_z = 6, 11, 3
 # brick_x, brick_y, brick_z = 6, 3, 11
 # brick_x, brick_y, brick_z = 3, 6, 11
-# brick_x, brick_y, brick_z = 3, 11, 6
+brick_x, brick_y, brick_z = 3, 11, 6
 # (просто раскоментировать нужную строку и проверить свой код)
 
-# TODO здесь ваш код
+if brick_x <= hole_x and brick_y <= hole_y:
+    print('ДА')
+elif brick_x <= hole_y and brick_y <= hole_x:
+    print('ДА')
+elif brick_z <= hole_x and brick_y <= hole_y:
+    print('ДА')
+elif brick_z <= hole_y and brick_y <= hole_x:
+    print('ДА')
+elif brick_z <= hole_x and brick_x <= hole_y:
+    print('ДА')
+elif brick_z <= hole_y and brick_y <= hole_x:
+    print('ДА')
+else:
+    print('НЕТ')
+
