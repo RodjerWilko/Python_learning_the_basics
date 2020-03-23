f8c2367aa42351d22ef9c5759118cff74b957459
Максим <ipkochetovmv@gmail.com>
2020-03-23 14:46:13 +0300

    урок 3. wall. поправил.

--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -6,16 +6,15 @@ import simple_draw
 # Нарисовать стену из кирпичей. Размер кирпича - 100х50
 # Использовать вложенные циклы for
 
-
-count = 1
+k = 0
+count = 0
 for i in range(0, 1000, 50):
     count += 1
-    for j in range(0, 1000, 100):
-        # TODO Перенесите это условие на уровень выше, во внешний цикл и меняйте
-        #  начальное значение диапазона. Сейчас проверка выполняется для каждого кирпича.
-        #  После переделки проверка будет проводиться для каждого ряда.
-        if count % 2 == 1:
-            j -= 50
+    if count % 2 == 1:
+        k = 50
+    else:
+        k = 0
+    for j in range(k, 1000, 100):
         left_bottom = simple_draw.Point(j, i)
         right_top = simple_draw.Point(j + 100, i + 50)
         simple_draw.rectangle(left_bottom=left_bottom, right_top=right_top, color=simple_draw.COLOR_DARK_RED, width=2)
