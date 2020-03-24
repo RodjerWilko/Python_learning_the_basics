bcb5e32fee905fc8013fd57134364a6ab0bc835b
Максим <ipkochetovmv@gmail.com>
2020-03-24 11:20:50 +0300

    урок 3. rainbow . поправил2.

--- lesson_003/06_rainbow.py
+++ lesson_003/06_rainbow.py
@@ -10,13 +10,12 @@ rainbow_colors = (sd.COLOR_RED, sd.COLOR_ORANGE, sd.COLOR_YELLOW, sd.COLOR_GREEN
 # Нарисовать радугу: 7 линий разного цвета толщиной 4 с шагом 5 из точки (50, 50) в точку (350, 450)
 x = [50, 50]
 y = [350, 450]
-# TODO Можно сразу назвать переменную color вместо i.
-for i in rainbow_colors:
+
+for color in rainbow_colors:
     start_point = sd.Point(x[0], x[1])
     end_point = sd.Point(y[0], y[1])
     x[0] += 5
     y[0] += 5
-    color = i
     sd.line(start_point=start_point, end_point=end_point, color=color, width=4)
 
 # Усложненное задание, делать по желанию.
--- lesson_003/07_wall.py
+++ lesson_003/07_wall.py
@@ -7,19 +7,13 @@ import simple_draw
 # Использовать вложенные циклы for
 
 k = 0
-count = 0
 for i in range(0, 1000, 50):
-    count += 1
     # Есть хитрый трюк, позволяющий убрать условие. В цикле на каждой итерации меняем k:
     # k = 50 - k. В результате k поочерёдно будет меняться с 50 на 0 и наоборот.
-    if count % 2 == 1:
-        k = 50
-    else:
-        k = 0
+    k = 50 - k
     for j in range(k, 1000, 100):
         left_bottom = simple_draw.Point(j, i)
         right_top = simple_draw.Point(j + 100, i + 50)
         simple_draw.rectangle(left_bottom=left_bottom, right_top=right_top, color=simple_draw.COLOR_DARK_RED, width=2)
-
 
 simple_draw.pause()
