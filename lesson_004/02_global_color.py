75cc3b10c5689362fd741b8575b947ca1b29da60
Максим <ipkochetovmv@gmail.com>
2020-03-25 21:40:06 +0300

    урок 4. global_color. поправил

--- lesson_004/02_global_color.py
+++ lesson_004/02_global_color.py
@@ -48,24 +48,22 @@ def draw_hexagon(_start_point, _angle, _length, _color):
         _start_point = v.end_point
 
 
-# TODO Если COLOR сделать словарём, то обрабатывать пользовательский ввод станет проще.
-#  Посмотрите пример в задании с днями месяца в 3 уроке.
-COLOR = [
-    {'name_color': 'red', 'code_color': sd.COLOR_RED},
-    {'name_color': 'orange', 'code_color': sd.COLOR_ORANGE},
-    {'name_color': 'yellow', 'code_color': sd.COLOR_YELLOW},
-    {'name_color': 'green', 'code_color': sd.COLOR_GREEN},
-    {'name_color': 'cyan', 'code_color': sd.COLOR_CYAN},
-    {'name_color': 'blue', 'code_color': sd.COLOR_BLUE},
-    {'name_color': 'purple', 'code_color': sd.COLOR_PURPLE}
-]
+COLOR = {
+    0: {'name_color': 'red', 'code_color': sd.COLOR_RED},
+    1: {'name_color': 'orange', 'code_color': sd.COLOR_ORANGE},
+    2: {'name_color': 'yellow', 'code_color': sd.COLOR_YELLOW},
+    3: {'name_color': 'green', 'code_color': sd.COLOR_GREEN},
+    4: {'name_color': 'cyan', 'code_color': sd.COLOR_CYAN},
+    5: {'name_color': 'blue', 'code_color': sd.COLOR_BLUE},
+    6: {'name_color': 'purple', 'code_color': sd.COLOR_PURPLE}
+}
 
 print('Возможные цвета:')
-for i in range(7):
+for i in COLOR:
     print("  ", i, ':', COLOR[i]['name_color'])
 while True:
     input_user = int(input('Введите желаемый цвет > '))
-    if input_user > 6 or input_user < 0:
+    if input_user not in COLOR:
         print('Вы ввели некорректный номер!')
         continue
     else:
