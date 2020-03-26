7a9515dff33dedbd038cdaaa14cb3cf41d6dacfe
Максим <ipkochetovmv@gmail.com>
2020-03-26 17:09:21 +0300

    урок 4. snowfall. Поправил

--- lesson_004/05_snowfall.py
+++ lesson_004/05_snowfall.py
@@ -17,45 +17,29 @@ N = 20
 # sd.user_want_exit()
 
 sd.resolution = (800, 600)
-length_snow = []
-list_xy = []
-# TODO Удобнее хранить координаты и параметры снежинок в одной структуре данных.
-#  Так работать с ними будет проще:
-#  snowflakes = [[0, 2, 4], [5, 6, 7], ...]
-#  список со списками:
-#  for x, y, length in snowflakes:
-#      point = Point(x, y)
-#      snowflake(point, length)
-#  или список со словарями:
-#  snowflakes = [{"x": 0, "y": 2, "length": 4}, {"x": 5, "y": 6, "length": 7}, ]
-#  for snowflake in snowflakes:
-#      point = Point(snowflake['x'], snowflake['y'])
-#      snowflake(point, snowflake["length"])
-#  snowflakes = {1: {"x": 0, "y": 2, "length": 4}, {"x": 5, "y": 6, "length": 7}, }
-#  for i, snowflake in snowflakes.items():
-#      point = Point(snowflake['x'], snowflake['y'])
-#      snowflake(point, snowflake["length"])
-for _ in range(N):
-    length_snow.append(sd.random_number(10, 100))
-for _ in range(N):
-    list_xy.append([sd.random_number(0, 800), sd.random_number(600, 800)])
+
+
+def new_snowflake():
+    _x = sd.random_number(0, 800)
+    _y = sd.random_number(700, 800)
+    _length = sd.random_number(10, 100)
+    _my_dict = {'x': _x, 'y': _y, 'length': _length}
+    snowflakes[i] = _my_dict
+
+
+snowflakes = {}
+
+for i in range(N):
+    new_snowflake()
 
 while True:
-    # TODO Лучше делать цикл по списку с параметрами снежинок.
-    #  В этом цикле нужен индекс элемента, поэтому можно применить
-    #  enumerate для списка или ключ словаря.
-    #  Объявление цикла будет немного сложнее, но можно будет существенно
-    #  упростить остальной код.
-    #  Пример для списка:
-    #  for i, (x, y, length) in enumerate(snowflake_param):
-    #  Пример для словаря, содержащего список параметров снежинки:
-    #  for i, (x, y, length) in snowflake_param.items():
-    #  Индекс i понадобится для обновления высоты и замены упавшей снежинки.
-    for i in range(N):
+    for i, snowflake in snowflakes.items():
         sd.start_drawing()
-        sd.snowflake(center=sd.get_point(*list_xy[i]), length=length_snow[i], color=sd.background_color)
-        list_xy[i][1] -= 10
-        sd.snowflake(center=sd.get_point(*list_xy[i]), length=length_snow[i], color=sd.COLOR_WHITE)
+        sd.snowflake(center=sd.get_point(snowflake['x'], snowflake['y']), length=snowflake['length'],
+                     color=sd.background_color)
+        snowflake['y'] -= 20
+        sd.snowflake(center=sd.get_point(snowflake['x'], snowflake['y']), length=snowflake['length'],
+                     color=sd.COLOR_WHITE)
         sd.finish_drawing()
         sd.sleep(0.1)
         if sd.user_want_exit():
@@ -79,21 +63,21 @@ sd.pause()
 # Результат решения см https://youtu.be/XBx0JtxHiLg
 
 while True:
-    for n, coord in enumerate(list_xy):
+    for i, snowflake in snowflakes.items():
         sd.start_drawing()
-        sd.snowflake(center=sd.get_point(*coord), length=length_snow[n], color=sd.background_color)
-        coord[1] -= 10
-        coord[0] += sd.random_number(-50, 50)
-        sd.snowflake(center=sd.get_point(*coord), length=length_snow[n], color=sd.COLOR_WHITE)
-        if coord[1] < length_snow[n]:
-            list_xy.append([sd.random_number(0, 800), sd.random_number(600, 2000)])
-            length_snow.append(sd.random_number(10, 100))
+        sd.snowflake(center=sd.get_point(snowflake['x'], snowflake['y']), length=snowflake['length'],
+                     color=sd.background_color)
+        snowflake['y'] -= 20
+        snowflake['x'] += sd.random_number(-50, 50)
+        sd.snowflake(center=sd.get_point(snowflake['x'], snowflake['y']), length=snowflake['length'],
+                     color=sd.COLOR_WHITE)
+        if snowflake['y'] < snowflake['length']:
+            new_snowflake()
             continue
-
 
         sd.finish_drawing()
         sd.sleep(0.01)
-        # TODO Этот код нужно вынести из внутреннего цикла, убрав лишние отступы.
-        if sd.user_want_exit():
-            break
+
+    if sd.user_want_exit():
+        break
 sd.pause()
