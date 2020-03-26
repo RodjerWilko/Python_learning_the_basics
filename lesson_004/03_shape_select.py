bbbd44f6cd36e86cebfe5390f5e113498595c1c0
Максим <ipkochetovmv@gmail.com>
2020-03-26 11:19:02 +0300

    урок 4. shape select. поправил

--- lesson_004/03_shape_select.py
+++ lesson_004/03_shape_select.py
@@ -2,6 +2,7 @@
 
 import simple_draw as sd
 
+
 # Запросить у пользователя желаемую фигуру посредством выбора из существующих
 #   вывести список всех фигур с номерами и ждать ввода номера желаемой фигуры.
 # и нарисовать эту фигуру в центре экрана
@@ -42,60 +43,46 @@ def draw_hexagon(_start_point, _angle, _length, _color):
         _start_point = v.end_point
 
 
-COLOR = [
-    {'name_color': 'red', 'code_color': sd.COLOR_RED},
-    {'name_color': 'orange', 'code_color': sd.COLOR_ORANGE},
-    {'name_color': 'yellow', 'code_color': sd.COLOR_YELLOW},
-    {'name_color': 'green', 'code_color': sd.COLOR_GREEN},
-    {'name_color': 'cyan', 'code_color': sd.COLOR_CYAN},
-    {'name_color': 'blue', 'code_color': sd.COLOR_BLUE},
-    {'name_color': 'purple', 'code_color': sd.COLOR_PURPLE}
-]
+COLOR = {
+    0: {'name_color': 'red', 'code_color': sd.COLOR_RED},
+    1: {'name_color': 'orange', 'code_color': sd.COLOR_ORANGE},
+    2: {'name_color': 'yellow', 'code_color': sd.COLOR_YELLOW},
+    3: {'name_color': 'green', 'code_color': sd.COLOR_GREEN},
+    4: {'name_color': 'cyan', 'code_color': sd.COLOR_CYAN},
+    5: {'name_color': 'blue', 'code_color': sd.COLOR_BLUE},
+    6: {'name_color': 'purple', 'code_color': sd.COLOR_PURPLE}
+}
 
-FIGURE = [
-    {'name_figure': 'треугольник'},
-    {'name_figure': 'квадрат'},
-    {'name_figure': 'пятигольник'},
-    {'name_figure': 'шестигольник'},
-]
-# TODO Будет удобнее создать словарь в значениях которого будут храниться
-#  названием фигур и ссылками на функции, а ключами словаря будут цифры,
-#  вводимые пользователем.
-#  Ссылки на функции, проще говоря их названия, можно поместить в структуру данных:
-#  список, кортеж, словарь, ... На примере списков это работает так:
-#  functs = [pentagon, hexagon, triangle]
-#  draw_function = functs[0]
-#  draw_function(start_point, start_angle, length
+FIGURE = {
+    0: {'name_figure': 'треугольник', 'func': draw_triangle},
+    1: {'name_figure': 'квадрат', 'func': draw_box},
+    2: {'name_figure': 'пятигольник', 'func': draw_pentagon},
+    3: {'name_figure': 'шестигольник', 'func': draw_hexagon},
+}
+
 print('Возможные цвета: ')
-for i in range(7):
+for i in COLOR:
     print("  ", i, ':', COLOR[i]['name_color'])
 while True:
     input_user = int(input('Введите желаемый цвет > '))
-    if input_user > 6 or input_user < 0:
+    if input_user not in COLOR:
         print('Вы ввели некорректный номер!')
         continue
     else:
         break
 print('Возможные фигуры: ')
-for i in range(4):
+for i in FIGURE:
     print("  ", i, ':', FIGURE[i]['name_figure'])
 while True:
     input_user2 = int(input('Введите желаемую фигуру > '))
-    if input_user2 > 3 or input_user2 < 0:
+    if input_user2 not in FIGURE:
         print('Вы ввели некорректный номер!')
         continue
     else:
         break
-if input_user2 == 0:
-    draw_triangle(_start_point=sd.get_point(100, 100), _angle=20, _length=100,
-                  _color=COLOR[input_user]['code_color'])
-elif input_user2 == 1:
-    draw_box(_start_point=sd.get_point(300, 300), _angle=40, _length=100,
-             _color=COLOR[input_user]['code_color'])
-elif input_user2 == 2:
-    draw_pentagon(_start_point=sd.get_point(100, 400), _angle=30, _length=100,
-                  _color=COLOR[input_user]['code_color'])
-elif input_user2 == 3:
-    draw_hexagon(_start_point=sd.get_point(400, 100), _angle=30, _length=100,
-                 _color=COLOR[input_user]['code_color'])
+
+draw_func = FIGURE[input_user2]['func']
+draw_func(_start_point=sd.get_point(100, 100), _angle=20, _length=100,
+          _color=COLOR[input_user]['code_color'])
+
 sd.pause()
