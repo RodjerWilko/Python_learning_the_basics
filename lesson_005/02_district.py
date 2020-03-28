10e27d47546cd8cd27cde14196627f040793554f
Максим <ipkochetovmv@gmail.com>
2020-03-28 14:22:22 +0300

    Урок 5. district. выполнено

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -5,7 +5,34 @@
 # подсказка: для вывода элементов списка через запятую можно использовать функцию строки .join()
 # https://docs.python.org/3/library/stdtypes.html#str.join
 
-# TODO здесь ваш код
+import district.central_street.house1.room1
+import district.central_street.house1.room2
+import district.central_street.house2.room1
+import district.central_street.house2.room2
+import district.soviet_street.house1.room1
+import district.soviet_street.house1.room2
+import district.soviet_street.house2.room1
+import district.soviet_street.house2.room2
+
+all_mans = []
+
+
+def add_to_list(_list):
+    for man in _list:
+        all_mans.append(man)
+
+
+add_to_list(district.central_street.house1.room1.folks)
+add_to_list(district.central_street.house1.room2.folks)
+add_to_list(district.central_street.house2.room1.folks)
+add_to_list(district.central_street.house2.room2.folks)
+add_to_list(district.soviet_street.house1.room1.folks)
+add_to_list(district.soviet_street.house1.room2.folks)
+add_to_list(district.soviet_street.house2.room1.folks)
+add_to_list(district.soviet_street.house2.room2.folks)
+
+print('На районе живут ', ', '.join(all_mans))
+
 
 
 
