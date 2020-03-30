0679e9a5cf2f041c3ed798c2557b7a330b8cb207
Максим <ipkochetovmv@gmail.com>
2020-03-30 18:48:02 +0300

    Урок 5. district. поправил

--- lesson_005/02_district.py
+++ lesson_005/02_district.py
@@ -14,28 +14,9 @@ import district.soviet_street.house1.room2
 import district.soviet_street.house2.room1
 import district.soviet_street.house2.room2
 
-all_mans = []
-
-
-def add_to_list(_list):
-    for man in _list:
-        all_mans.append(man)
-
-# TODO Можно получить список жителей простым суммированием
-#  импортируемых списков.
-#  Это будет проще и бестрее, чем добавлять в список по одному элементу.
-add_to_list(district.central_street.house1.room1.folks)
-add_to_list(district.central_street.house1.room2.folks)
-add_to_list(district.central_street.house2.room1.folks)
-add_to_list(district.central_street.house2.room2.folks)
-add_to_list(district.soviet_street.house1.room1.folks)
-add_to_list(district.soviet_street.house1.room2.folks)
-add_to_list(district.soviet_street.house2.room1.folks)
-add_to_list(district.soviet_street.house2.room2.folks)
+all_mans = district.central_street.house1.room1.folks + district.central_street.house1.room2.folks + \
+           district.central_street.house2.room1.folks + district.central_street.house2.room2.folks + \
+           district.soviet_street.house1.room1.folks + district.soviet_street.house1.room2.folks + \
+           district.soviet_street.house2.room1.folks + district.soviet_street.house2.room2.folks
 
 print('На районе живут ', ', '.join(all_mans))
-
-
-
-
-
