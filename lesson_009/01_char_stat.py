fe294ef0cacb15df838dba814b7e88bb84620bdc
Максим <ipkochetovmv@gmail.com>
2020-04-10 19:52:17 +0300

    урок 9. статистка.

--- lesson_009/01_char_stat.py
+++ lesson_009/01_char_stat.py
@@ -21,7 +21,62 @@
 # Упорядочивание по частоте - по убыванию. Ширину таблицы подберите по своему вкусу
 # Требования к коду: он должен быть готовым к расширению функциональности. Делать сразу на классах.
 
-# TODO здесь ваш код
+import os
+import zipfile
+
+
+class StatDescending:
+    """Упорядочивание по частоте - по убыванию"""
+
+    def __init__(self, file_name):
+        self.file_name = file_name
+
+    def unzip(self):
+        file_name = os.path.normpath(self.file_name)
+        if file_name.endswith('.zip'):
+            file = zipfile.ZipFile(file_name, 'r')
+            for filename in file.namelist():
+                file.extract(filename)
+        self.file_name = filename
+
+    def get_stat_dict(self):
+        if self.file_name.endswith('.zip'):
+            self.unzip()
+        chars = {}
+        with open(self.file_name, 'r') as file:
+            for line in file:
+                for char in line:
+                    if char.isalpha():
+                        if char in chars:
+                            chars[char] += 1
+                        else:
+                            chars[char] = 1
+        return chars
+
+    def sort_dict(self):
+        chars_list = list(self.get_stat_dict().items())
+        chars_list.sort(key=lambda i: i[1], reverse=True)
+        return chars_list
+
+    def print_stat(self):
+        chars_list = self.sort_dict()
+        total = 0
+        print('+----------+----------+')
+        print('|{char_n:^10}|{quantity_n:^10}|'.format(char_n='буква', quantity_n='частота'))
+        print('+----------+----------+')
+
+        for char in chars_list:
+            print(f'|{char[0]:^10}|{char[1]:^10}|')
+            total += char[1]
+
+        print('+----------+----------+')
+        print('|{total_nam:^10}|{total_var:^9} |'.format(total_nam='итого', total_var=total))
+        print('+----------+----------+')
+
+
+char_stat = StatDescending(file_name='python_snippets\\voyna-i-mir.txt.zip')
+char_stat.print_stat()
+
 
 # После выполнения первого этапа нужно сделать упорядочивание статистики
 #  - по частоте по возрастанию
@@ -30,3 +85,39 @@
 # Для этого пригодится шаблон проектирование "Шаблонный метод"
 #   см https://goo.gl/Vz4828
 #   и пример https://gitlab.skillbox.ru/vadim_shandrinov/python_base_snippets/snippets/4
+
+class StatAscending(StatDescending):
+    """Упорядочивание по частоте - по возрастанию"""
+
+    def sort_dict(self):
+        chars_list = list(self.get_stat_dict().items())
+        chars_list.sort(key=lambda i: i[1], reverse=False)
+        return chars_list
+
+
+class StatAlphaAscending(StatDescending):
+    """Упорядочивание по алфавиту - по возрастанию"""
+
+    def sort_dict(self):
+        chars_list = list(self.get_stat_dict().items())
+        chars_list.sort(key=lambda i: i[0], reverse=False)
+        return chars_list
+
+
+class StatAlphaDescending(StatDescending):
+    """Упорядочивание по алфавиту - по убыванию"""
+
+    def sort_dict(self):
+        chars_list = list(self.get_stat_dict().items())
+        chars_list.sort(key=lambda i: i[0], reverse=True)
+        return chars_list
+
+
+char_stat2 = StatAscending(file_name='python_snippets\\voyna-i-mir.txt.zip')
+char_stat2.print_stat()
+
+char_stat3 = StatAlphaAscending(file_name='python_snippets\\voyna-i-mir.txt.zip')
+char_stat3.print_stat()
+
+char_stat4 = StatAlphaDescending(file_name='python_snippets\\voyna-i-mir.txt.zip')
+char_stat4.print_stat()
