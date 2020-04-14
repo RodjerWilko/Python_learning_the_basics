6ebdcf3c76fe09242b4cb5a1dd1b9c1f3ac145d4
Максим <ipkochetovmv@gmail.com>
2020-04-14 13:04:49 +0300

    урок 10. День сурка

--- lesson_010/02_groundhog_day.py
+++ lesson_010/02_groundhog_day.py
@@ -16,9 +16,72 @@
 # кармы до уровня ENLIGHTENMENT_CARMA_LEVEL. Исключения обработать и записать в лог.
 # При создании собственных исключений максимально использовать функциональность
 # базовых встроенных исключений.
+import random
 
 ENLIGHTENMENT_CARMA_LEVEL = 777
 
-# TODO здесь ваш код
+
+class IamGodError(Exception):
+
+    def __str__(self):
+        return 'IamGodError'
+
+
+class DrunkError(Exception):
+    def __str__(self):
+        return 'DrunkError'
+
+
+class CarCrashError(Exception):
+    def __str__(self):
+        return 'CarCrashError'
+
+
+class GluttonyError(Exception):
+    def __str__(self):
+        return 'GluttonyError'
+
+
+class DepressionError(Exception):
+    def __str__(self):
+        return 'DepressionError'
+
+
+class SuicideError(Exception):
+    def __str__(self):
+        return 'SuicideError'
+
+
+exceptions = [IamGodError, DrunkError, CarCrashError, GluttonyError,
+              DepressionError, SuicideError]
+
+
+def one_day():
+    dice = random.randint(0, 13)
+    if dice == 12:
+        except_ = exceptions[random.randint(0, 5)]
+        raise except_
+    else:
+        return random.randint(1, 8)
+
+
+total_carma = 0
+total_days = 0
+
+while True:
+    if total_carma >= ENLIGHTENMENT_CARMA_LEVEL:
+        print(f'Поздравляю за {total_days} дней Ваша карма стала 777 или выше и Вы вышли из бесконечного цикла!!!')
+        break
+    else:
+        try:
+            print(total_carma)
+            carma = one_day()
+            total_days += 1
+            total_carma += carma
+        except Exception as exc:
+            print(f'Вылетело исключение {exc}')
+            with open(file='error_log.txt', mode='a') as f:
+                f.write(f'{str(exc)}\r')
+
 
 # https://goo.gl/JnsDqu
