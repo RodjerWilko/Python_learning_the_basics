6fdb09e57bb462197238d180454d219b33307702
Максим <ipkochetovmv@gmail.com>
2020-04-14 23:18:03 +0300

    урок 10. проверка файла и логирование

--- lesson_010/03_registration_log.py
+++ lesson_010/03_registration_log.py
@@ -21,5 +21,51 @@
 # - поле емейл НЕ содержит @ и .(точку): NotEmailError (кастомное исключение)
 # - поле возраст НЕ является числом от 10 до 99: ValueError
 # Вызов метода обернуть в try-except.
+import os
 
-# TODO здесь ваш код
+
+class NotNameError(Exception):
+    pass
+
+
+class NotMailError(Exception):
+    pass
+
+
+def check_valid(stroke):
+    stroke = stroke.replace('\n', '').split(' ')
+    if len(stroke) < 3:
+        raise ValueError('Не указан один из параметров')
+    elif not stroke[0].isalpha():
+        raise NotNameError('Указано некорректное имя')
+    elif '@' not in stroke[1] and '.' not in stroke[0]:
+        raise NotMailError('Указана некорректныая почта')
+    elif int(stroke[2]) > 99 or int(stroke[2]) < 10:
+        raise ValueError('Указан некорректный возраст')
+    else:
+        return True
+
+
+PATH = 'registrations.txt'
+total_strokes = 0
+total_good_log = 0
+total_bad_log = 0
+
+if os.path.exists(PATH):
+    with open(file='registrations.txt', mode='r', encoding='utf-8')as ff:
+        for line in ff:
+            total_strokes += 1
+            try:
+                if check_valid(line):
+                    with open(file='registrations_good.log', mode='a', encoding='utf8') as log_g:
+                        log_g.write(line)
+                        total_good_log += 1
+            except Exception as exc:
+                with open(file='registrations_bad.log', mode='a', encoding='utf8') as log_b:
+                    log_b.write(f'{line[:-1]} - {exc}\n')
+                    total_bad_log += 1
+
+    if (total_good_log + total_bad_log) == total_strokes:
+        print('Проверка файла прошла успешно!')
+else:
+    print(f'Такого пути : {PATH} - не сущестует')
