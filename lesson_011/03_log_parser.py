e8622cc640cf798e48cbcbd76241a8af7f6c109e
Максим <ipkochetovmv@gmail.com>
2020-04-17 08:19:50 +0300

    урок 11. log_parser

--- lesson_011/03_log_parser.py
+++ lesson_011/03_log_parser.py
@@ -13,5 +13,42 @@
 # на консоли должно появится что-то вроде
 #
 # [2018-05-17 01:57] 1234
+import os
 
-# TODO здесь ваш код
+PATH = os.path.normpath('events.txt')
+
+
+def pars_gen():
+    my_log_list = []
+    my_log_dict = []
+    with open(file=PATH, mode='r') as file:
+        for line in file:
+            if line.split(' ')[2].replace('\n', '') == 'NOK':
+                my_log_list.append(line.replace('\n', ''))
+    for day in my_log_list:
+        my_dict = {
+            'day': day,
+            'count': 0
+        }
+        my_log_dict.append(my_dict)
+
+    log_sum = []
+    for day in my_log_dict:
+        if log_sum:
+            for i, day2 in enumerate(log_sum):
+                if day['day'][1:17] == day2['day'][1:17]:
+                    day2['count'] += 1
+                else:
+                    if i == (len(log_sum) - 1):
+                        log_sum.append(day)
+        else:
+            day['count'] = 1
+            log_sum.append(day)
+
+    for x in log_sum:
+        yield x.values()
+
+
+grouped_events = pars_gen()
+for group_time, event_count in grouped_events:
+    print(f'[{group_time[1:17]}] {event_count}')
