fba5c9c91c1cd92a2691b61906558ca07f7da892
Максим <ipkochetovmv@gmail.com>
2020-04-17 16:52:45 +0300

    урок 11. log_decorator_error

--- lesson_011/04_error_log_decorator.py
+++ lesson_011/04_error_log_decorator.py
@@ -6,20 +6,50 @@
 # Имя файла лога - function_errors.log
 # Формат лога: <имя функции> <параметры вызова> <тип ошибки> <текст ошибки>
 # Лог файл открывать каждый раз при ошибке в режиме 'a'
+import os
+
+PATH_OUT = os.path.normpath('function_errors.log')
 
 
-def log_errors(func):
-    pass
-    # TODO здесь ваш код
+def log_errors(func):  # ОБЫЧНЫЙ ДЕКОРАТОР
+    def surrogate(param):
+        try:
+            result = func(param)
+        except Exception as e:
+            with open(file=PATH_OUT, mode='a', encoding='UTF-8') as ff:
+                ff.write(
+                    f'имя функции:<{func.__name__}> параметры вызова: <{param} >'
+                    f' тип ошибки:  <{e}> текст ошибки :<{e.args}\n>')
+
+        return result(param)
+
+    return surrogate
 
 
-# Проверить работу на следующих функциях
-@log_errors
+def log_errors_file(file):  # УСЛОЖНЕННЫЙ ДЕКОРАТОР
+    def log_errors(func):
+        def surrogate(param):
+            try:
+                result = func(param)
+            except Exception as e:
+                with open(file=os.path.normpath(file), mode='a', encoding='UTF-8') as ff:
+                    ff.write(
+                        f'имя функции:<{func.__name__}> параметры вызова: <{param} >'
+                        f' тип ошибки:  <{e}> текст ошибки :<{e.args}\n>')
+
+            return result(param)
+
+        return surrogate
+
+    return log_errors
+
+
+@log_errors_file('function_errors.log')
 def perky(param):
     return param / 0
 
 
-@log_errors
+@log_errors_file('function_errors.log')
 def check_line(line):
     name, email, age = line.split(' ')
     if not name.isalpha():
@@ -38,13 +68,14 @@ lines = [
     'Земфира 86',
     'Равшан wmsuuzsxi@mail.ru 35',
 ]
+
 for line in lines:
     try:
         check_line(line)
     except Exception as exc:
         print(f'Invalid format: {exc}')
-perky(param=42)
 
+perky(param=42)
 
 # Усложненное задание (делать по желанию).
 # Написать декоратор с параметром - именем файла
@@ -52,4 +83,3 @@ perky(param=42)
 # @log_errors('function_errors.log')
 # def func():
 #     pass
-
